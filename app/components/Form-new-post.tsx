9e8bbb8024e31daf472918dd4d8a0b6c5d7869d8
kinDreno <sepriothofficial@gmail.com>
2024-10-19 08:17:27 -0700

    #

---
+++ app/components/Form-new-post.tsx
@@ -0,0 +1,47 @@
+  'use client'
+  import React, { ChangeEvent, FormEvent, useState } from 'react'
+  import { FormData } from '../types/blog'
+  
+  const FormNewPost = () => {
+
+    const [formData, setFormData] = useState<FormData>({
+      title: '',
+      content: '',
+    })
+
+  const handleChange = (e : 
+    ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+  e.preventDefault()
+  const {name, value} = e.target;
+  setFormData({...formData, [name]: value,});
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  console.log(formData)
+  }
+    return (
+      <form className='max-w-md mx-auto p-4' onSubmit={handleSubmit} action="text">
+          <div className='mb-4'>
+              <input type="text" className='duration-500 border-2 p-4 w-full border-slate-900 rounded-lg
+              focus:ring'
+              placeholder='Enter the Title' name='title' value={formData.title} 
+              onChange={handleChange}
+              />
+          </div>
+
+          <div className='mb-4'>
+
+          <textarea 
+    className=' resize-y border-2 p-4 w-full min-w-8 border-slate-900 rounded-lg focus:ring'
+    placeholder='Enter the Content'
+    name='content' value={formData.content} onChange={handleChange}
+  />
+          </div>
+          <button type='submit' className='bg-slate-900 hover:bg-slate-700 font-bold w-full text-white p-4 rounded-md' >Submit</button>
+      </form>
+    )
+  }
+
+  export default FormNewPost
---
+++ app/components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+const Header = () => {
+  return (
+    <>
+    <header className="bg-slate-800 p-4">
+        <nav className="flex justify-between items-center max-w-4xl mx-auto">
+            <Link className="text-white text-2xl font-bold" href={"/"}>My Blogs</Link>
+            <ul className="flex space-x-3">
+                {[['/api/auth/signin', 'Log In'], ['/blogs', 'Blogs']].map(([href, name], index) => {
+                    return <li key={index}><Link className="duration-500 text-white hover:underline" href={href}>{name}</Link></li>
+                })}
+            </ul>
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default Header
